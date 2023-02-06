4040eacb445797795e32fc189ee3c34ca2a83d1d
Josie Worner <wornerjl@gmail.com>
2023-02-05 20:25:49 -0500

    finished api call for information and added api call for images

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 
 async function getImage(img_index) {
     let id = document.getElementsByTagName('p')[img_index].getAttribute('id');
-    let request = new Request(`https://api.artic.edu/api/v1/artworks/${id}?fields=title,thumbnail,artist_display,place_of_origin,medium_display,credit_line`, {
+    let request = new Request(`https://api.artic.edu/api/v1/artworks/${id}?fields=image_id,title,thumbnail,artist_display,place_of_origin,medium_display,credit_line`, {
         method: 'GET'
     });
 
@@ -25,34 +25,18 @@ async function getImage(img_index) {
 
     console.log(response);
 
-    let text = document.getElementsByTagName('p')[img_index]
+    let text = document.getElementsByTagName('p')[img_index];
     text.innerHTML = `<span class="fw-bold">Title:</span> ${response.data.title}<br /><br />
-                    <span class="fw-bold">Artist:</span> ${response.data.artist_display}`
+                    <span class="fw-bold">Artist:</span> ${response.data.artist_display}<br /><br />
+                    <span class="fw-bold">Country of Origin:</span> ${response.data.place_of_origin}<br /><br />
+                    <span class="fw-bold">Medium:</span> ${response.data.medium_display}<br /><br />
+                    <span class="fw-bold">Credit:</span> ${response.data.credit_line}`;
 
-    let text_alt = document.getElementsByTagName('img')[img_index]
-    text_alt.alt = `${response.data.thumbnail.alt_text}`
+    let img = document.getElementsByTagName('img')[img_index];
+    img.alt = `${response.data.thumbnail.alt_text}`;
+
+    img.src = `${response.config.iiif_url}/${response.data.image_id}/full/843,/0/default.jpg`
 };
-
-// async function getInfo(p_index) {
-//     let id = document.getElementsByTagName('p')[p_index].getAttribute('id');
-//     console.log(id)
-//     let request = new Request(`https://api.artic.edu/api/v1/artworks/${id}?fields=title,thumbnail,artist_display,place_of_origin,medium_display,credit_line`, {
-//         method: 'GET'
-//     });
-
-//     let result = await fetch(request);
-
-//     let response = await result.json();
-
-//     console.log(response);
-
-//     console.log(response.data.title);
-//     console.log(response.data.thumbnail.alt_text);
-//     console.log(response.data.artist_display);
-//     console.log(response.data.place_of_origin);
-//     console.log(response.data.medium_display);
-//     console.log(response.data.credit_line);
-// };
 
 function requestImages() {
     let arr = document.getElementsByTagName('p');
